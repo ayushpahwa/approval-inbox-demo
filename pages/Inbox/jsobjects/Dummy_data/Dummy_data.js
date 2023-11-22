009cadc7c0c280f4d7c7c8164b1223d83050ef13
Ayush Pahwa <ayush@appsmith.com>
2023-11-22 23:49:19 +0700

    Fixed approved status

--- pages/Inbox/jsobjects/Dummy_data/Dummy_data.js
+++ pages/Inbox/jsobjects/Dummy_data/Dummy_data.js
@@ -15,11 +15,11 @@ export default {
 	async filterData () {
 		const rawData = await appsmith.store.rawData;
 		const statusFilter = Status_filter.selectedOptionValue;
-		const responseFilter = statusFilter === "pending"? "pending":Response_filter.selectedOptionValue;
+		const responseFilter = statusFilter === "pending"? "pending":Response_filter.selectedOptionValue.toLowerCase();
 		const filteredData = await rawData.filter((datum,index)=>{
 			console.log(datum.resolution)
 			const resolutionStatusFilterOutput = datum.resolutionStatus.toLowerCase() === statusFilter;
-			const resolutionFilterOutput = (responseFilter==="pending"?true:datum.resolution === responseFilter);
+			const resolutionFilterOutput = (["pending","all"].includes(responseFilter)?true:datum.resolution?.toLowerCase() === responseFilter);
 			const workflowIdCheck = datum.workflowId === "655e29723318161e81b0382a";
 			return (
 				resolutionStatusFilterOutput && resolutionFilterOutput && workflowIdCheck
